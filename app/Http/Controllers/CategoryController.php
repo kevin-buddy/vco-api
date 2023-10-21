48bd8df0943e5d8c93d69802c5db0f0324714cc9
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2023-10-21 16:21:33 +0700

    limit offset get data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,16 +5,89 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
     {
-        return CategoryResource::collection(Category::all());
+        $validator = Validator::make($request->all(), [
+            'limit' => 'required|numeric|min:0',
+            'offset' => 'required|numeric|min:0'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        return CategoryResource::collection(
+            Category::offset($request->offset)
+                ->limit($request->limit)
+                ->get()
+        );
     }
 
-    public function show(Category $category)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        return CategoryResource::make($category);
+        $request->validate([
+            'name' => 'required|max:20'
+        ]);
+        $new_category = new Category;
+        $new_category->name = $request->name;
+        $new_category->save();
+        return response("New Category Created", 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return CategoryResource::make(Category::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|max:20'
+        ]);
+        $update_category = Category::findOrFail(1);
+        $update_category->name = $request->name;
+        $update_category->save();
+        return response("Category Updated", 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Category::where('id', '=', $id)->delete();
+        return response("Category Deleted", 200);
     }
 }
--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -5,16 +5,101 @@ namespace App\Http\Controllers;
 use App\Http\Resources\IngredientResource;
 use App\Models\Ingredient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class IngredientController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
     {
-        return IngredientResource::collection(Ingredient::all());
+        $validator = Validator::make($request->all(), [
+            'limit' => 'required|numeric|min:0',
+            'offset' => 'required|numeric|min:0'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        return IngredientResource::collection(
+            Ingredient::offset($request->offset)
+                ->limit($request->limit)
+                ->get()
+        );
     }
 
-    public function show(Ingredient $ingredient)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        return IngredientResource::make($ingredient);
+        $request->validate([
+            'name' => 'required|alpha|max:20',
+            'description' => 'required|alpha_num|max:225',
+            'image' => 'required|max:225',
+            'ref_benefit' => 'required',
+        ]);
+        $new_ingredient = new Ingredient;
+        $new_ingredient->name = $request->name;
+        $new_ingredient->description = $request->description;
+        $new_ingredient->image = $request->image;
+        $new_ingredient->ref_benefit = $request->ref_benefit;
+        $new_ingredient->save();
+        return response("New Ingredient Created", 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return IngredientResource::make(Ingredient::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|alpha|max:20',
+            'description' => 'required|alpha_num|max:225',
+            'image' => 'required|max:225',
+            'ref_benefit' => 'required',
+        ]);
+        $update_ingredient = Ingredient::findOrFail(1);
+        $update_ingredient->name = $request->name;
+        $update_ingredient->description = $request->description;
+        $update_ingredient->image = $request->image;
+        $update_ingredient->ref_benefit = $request->ref_benefit;
+        $update_ingredient->save();
+        return response("Ingredient Updated", 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Ingredient::where('id', '=', $id)->delete();
+        return response("Ingredient Deleted", 200);
     }
 }
